486dd00d9f3c586a0fbebb6f8905f66e005b6b18
seigo07 <seven_8s@icloud.com>
2023-02-19 03:46:37 +0000

    Refactor

--- src/p1.py
+++ src/p1.py
@@ -1,27 +1,25 @@
 import pandas as pd
-from sklearn.preprocessing import MinMaxScaler, LabelEncoder, StandardScaler
+from src.p1_functions import *
+from sklearn.preprocessing import LabelEncoder, StandardScaler
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import accuracy_score, balanced_accuracy_score, confusion_matrix, precision_score, recall_score, \
-    f1_score, classification_report
-import numpy as np
 
-from src.p1_functions import *
+csv_file = "../drybeans.csv"
 
 # Part1
 
 # Read csv
-df = pd.read_csv("../drybeans.csv")
+df = pd.read_csv(csv_file)
 # print(df)
 
 # Check outliers
 
 # df['Area'] = remove_outliers(df, 'Area')
-df = remove_outliers(df)
+# df = remove_outliers(df)
 # print(df)
 
 # Check missing values
-check_missing_values(df)
+# check_missing_values(df)
 
 # Check and delete duplicated rows
 df = check_and_delete_duplicated_rows(df)
@@ -29,6 +27,7 @@ df = check_and_delete_duplicated_rows(df)
 
 x = df.loc[:, 'Area':'ShapeFactor4']
 df.loc[:, 'Class'] = LabelEncoder().fit_transform(df.loc[:, 'Class'])
+print("Count negative number = ", str(df.agg(lambda x: sum(x < 0)).sum()))
 y = df.loc[:, 'Class']
 # print(x)
 # print(y)
@@ -49,36 +48,14 @@ x_train, x_test, y_train, y_test = train_test_split(x_std, y, stratify=y)
 model = LogisticRegression(penalty="none", class_weight=None)
 # model = LogisticRegression(penalty="none", class_weight='balanced')
 model.fit(x_train, y_train)
-
 pred = model.predict(x_test)
-# score = model.score(x_test, y_test)
-# print('Accuracy rate = ', score)
 # print('pred = ', pred)
 # print('decision_function = ', model.decision_function(x_test))
 # print('predict_proba = ', model.predict_proba(x_test))
 
-# print('classification accuracy = ', accuracy_score(y_test, pred))
-# print('accuracy = ', accuracy_score(y_test, pred.round(), normalize=True))
-# print('balanced accuracy = ', balanced_accuracy_score(y_test, pred))
-# print('confusion matrix = \n', confusion_matrix(y_test, pred))
-# print('precision = ', precision_score(y_test, pred, average=None))
-# print('precision micro = ', precision_score(y_test, pred, average='micro'))
-# print('precision macro = ', precision_score(y_test, pred, average='macro'))
-# print('recall = ', recall_score(y_test, pred, average=None))
-# print('recall micro = ', recall_score(y_test, pred, average='micro'))
-# print('recall macro = ', recall_score(y_test, pred, average='macro'))
-# print('f1 score = ', f1_score(y_test, pred, average=None))
-# print('classification report = ', classification_report(y_test, pred))
+# Part3
 
-# sum of diagonal elements of confusion matrix / sum of all elements of the confusion matrix
-c = confusion_matrix(y_test, pred)
-accuracy = c.trace() / c.sum()
-# print('classification accuracy = ', accuracy)
-
-# sum of recall_score / size of recall_score
-r = recall_score(y_test, pred, average=None)
-accuracy = r.sum() / r.size
-# print('balanced accuracy = ', accuracy)
+output_result(y_test, pred)
 
 #%%
 
--- src/p1_functions.py
+++ src/p1_functions.py
@@ -1,31 +1,23 @@
+from sklearn.metrics import accuracy_score, balanced_accuracy_score, confusion_matrix, precision_score, recall_score, \
+    f1_score, classification_report
+
 # def remove_outliers(df):
 #     for col in df.columns:
 #         # Exclude categorical variables
 #         if col == "Class":
 #             break
-#
-#         # Review descriptive statistics
-#         # print(df[col].describe())
-#
-#         # Show graph
-#         # sns.distplot(df[col])
-#         # sns.boxplot(df['Area'])
-#         # sns.scatterplot(x="Area", data=df)
-#
+#         if (df[col].mean() + (df[col].std()) * 3) > df[col].max():
+#             break
 #         # Specify the quantile
 #         # Find the value at 50% of the second quartile of the median
 #         q_50 = df[col].quantile(0.5)
-#         # print("The value at 50% of the second quartile of the median = ", q_50)
-#
-#         # Get 95% quantile values and remove outliers
+#         # Get 95% quantile values
 #         q_95 = df[col].quantile(0.95)
-#         # print("The value at 95% of the second quartile of the median = ", q_95)
-#
 #         # Extract data with values less than 95% order
 #         new_df = df.query(f'{col} < @q_95')
 #         df[col] = new_df[col]
-#
 #     return df
+
 
 def check_missing_values(df):
     print("Count non-null values per row and column")
@@ -49,3 +41,28 @@ def check_and_delete_duplicated_rows(df):
     # Remove rows with duplicates on all columns
     return df.drop_duplicates()
     # print("df length after removing = ", len(df))
+
+
+def output_result(y_test, pred):
+    print('accuracy = ', accuracy_score(y_test, pred.round(), normalize=True))
+    print('classification accuracy = ', accuracy_score(y_test, pred))
+    print('balanced accuracy = ', balanced_accuracy_score(y_test, pred))
+    print('confusion matrix = \n', confusion_matrix(y_test, pred))
+    print('precision = ', precision_score(y_test, pred, average=None))
+    print('precision micro = ', precision_score(y_test, pred, average='micro'))
+    print('precision macro = ', precision_score(y_test, pred, average='macro'))
+    print('recall = ', recall_score(y_test, pred, average=None))
+    print('recall micro = ', recall_score(y_test, pred, average='micro'))
+    print('recall macro = ', recall_score(y_test, pred, average='macro'))
+    print('f1 score = ', f1_score(y_test, pred, average=None))
+    print('classification report = ', classification_report(y_test, pred))
+
+    # sum of diagonal elements of confusion matrix / sum of all elements of the confusion matrix
+    c = confusion_matrix(y_test, pred)
+    accuracy = c.trace() / c.sum()
+    # print('classification accuracy = ', accuracy)
+
+    # sum of recall_score / size of recall_score
+    r = recall_score(y_test, pred, average=None)
+    accuracy = r.sum() / r.size
+    # print('balanced accuracy = ', accuracy)
